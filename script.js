13c25f3af66d88c638c71588260b1401ef8bda47
scarlett-troncoso <scar.labussieri@gmail.com>
2024-08-21 16:11:25 +0200

    changes functions and refactoring

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
-/*
-Milestone 1
-Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
-
-Milestone 2
-Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
-
-Milestone 3
+/* TRACCIA:
 Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
-
-BONUS
-1 - modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
-2 - popolare le options della select della milestone 3 dinamicamente.
-
-Consigli del giorno
-Come sempre, iniziamo prima di tutto dall'analisi e comprensione della consegna. Scomponiamo il problema in micro-passaggi logici che solamente in un secondo momento trasformeremo in codice.
-
-Le icone presenti nella struttura dati fanno riferimento alla nota libreria Font Awesome, perciò come prima cosa assicuriamoci di aver inserito il link alla cdn nell'head della pagina. Dopodiché, basandoci sul codice di riferimento nel sito di Font Awesome, analizziamo come è formato il tag <i> di un'icona qualsiasi, in particolare focalizziamoci sulle classi.
-
-Come possiamo usare i dati presenti nella nostra struttura dati per creare l'elemento html nel modo corretto e visualizzare l'icona in pagina?
-Inizialmente può essere sufficiente stampare dei semplici div, senza alcuno stile, con all'interno l'icona e uno span con il nome.
-Solamente quando la parte logica è completa, ci dedichiamo al css.
-
 */
+
+/* Dati */
 
 const iconsDati = [
 	{
@@ -140,35 +121,38 @@ const iconsDati = [
 
 console.log(iconsDati);
 
+/* Selezionare il TAG contenitore degli icons, HTML */
+
 const containerDiv = document.querySelector('.container .row')
 
-iconsDati.forEach((icon) => {
-	console.log(`${icon.name} ${icon.prefix + icon.type} ${icon.family} ${icon.color}`);
 
-	const iconsDiv =
-	`<div class="col-1 m-3">
-		<div class="${icon.prefix + icon.name} ${icon.family} ${icon.color}">
-		</div>
-		<p class="m-0">${icon.name}</p>
-	</div>`
-	//containerDiv.style.color = icon.color
-	containerDiv.insertAdjacentHTML('beforeend', iconsDiv)	
-})
+/* Funzione per stampare in pagina gli icons */
+
+function stampIcon(icons) {
+
+	containerDiv.innerHTML = ""
+
+	icons.forEach((icon) => {
+
+		const iconsDivAnimal =
+		`<div class="col-1 m-3">
+			<div class="${icon.prefix + icon.name} ${icon.family} ${icon.color}">
+			</div>
+			<p class="${icon.color}">${icon.name}</p>
+		</div>`
+		containerDiv.insertAdjacentHTML('beforeend', iconsDivAnimal)
+	})
+}
+
+stampIcon(iconsDati);
 
 
-/*
-Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
-*/
-
-//function per select
-
-console.log('--------------------------');
+//Select HTML
 
 const iconSelect = document.getElementById("select_icon")
 
-//const typeIcon = document.querySelector('.form-select')  //prima era:   .icon_type  //non é necesario quest costante
 
-// Filtrare segun il tipo
+// Filtrare per tipo
 
 const icon_animal = iconsDati.filter(icon => icon.type === 'animal')
 
@@ -177,70 +161,34 @@ const icon_vegetable = iconsDati.filter(icon => icon.type === 'vegetable')
 const icon_user = iconsDati.filter(icon => icon.type === 'user')
 
 
+// Evento, alla selezione di una opzione della select, chiamare la function
 
-console.log('--------------------------');
+iconSelect.addEventListener('change', funSelector());
 
-iconSelect.addEventListener('change', funSelector);
 
-function funSelector(e){
-    if (iconSelect.value === 'animal') {
-	console.log(icon_animal);
+/* Funzione per selezionare gli icons */
 
-	containerDiv.innerHTML = ""
+function funSelector(){
+    if (iconSelect.value === 'animal') { // Select option "Animal"
+	
+		console.log(icon_animal);
 
-	icon_animal.forEach((iconAn) => {
+		stampIcon(icon_animal);
 
-	const iconsDivAnimal =
-    `<div class="col-1 m-3">
-        <div class="${iconAn.prefix + iconAn.name} ${iconAn.family}">
-        </div>
-        <p>${iconAn.name}</p>
-    </div>`
-	containerDiv.style.color = iconAn.color
-    containerDiv.insertAdjacentHTML('beforeend', iconsDivAnimal)
+	} else if (iconSelect.value === 'vegetable') { // Select option "Vegetable"
+			
+		console.log(icon_vegetable);
 
-		})} else if (iconSelect.value === 'vegetable') {
-			console.log(icon_vegetable);
+			stampIcon(icon_vegetable);
 
-			containerDiv.innerHTML = ""
-
-			icon_vegetable.forEach((iconVeg) => {
-
-			const iconsDivVegetable =
-			`<div class="col-1 m-3">
-				<div class="${iconVeg.prefix + iconVeg.name} ${iconVeg.family}">
-				</div>
-				<p>${iconVeg.name}</p>
-			</div>`
-			containerDiv.style.color = iconVeg.color
-			containerDiv.insertAdjacentHTML('beforeend', iconsDivVegetable) 
-
-			})} else if (iconSelect.value === 'user') {
+			} else if (iconSelect.value === 'user') { // Select option "User"
+				
 				console.log(icon_user);
 
-				containerDiv.innerHTML = ""
-	
-				icon_user.forEach((iconUs) => {
-	
-				const iconsDivUser =
-				`<div class="col-1 m-3">
-					<div class="${iconUs.prefix + iconUs.name} ${iconUs.family}">
-					</div>
-					<p>${iconUs.name}</p>
-				</div>`
-				containerDiv.style.color = iconUs.color
-				containerDiv.insertAdjacentHTML('beforeend', iconsDivUser)
-		
-				})}
+				stampIcon(icon_user);
+
+				} else { // Select option "All"
+
+					stampIcon(iconsDati);
+				}
 			};
-
-/* BONUS
-const cod = Math.floor(Math.random( ) * 10)
-console.log(cod);
-
-const letras = ['A', 'B', 'C', 'D', 'E', 'F']
-
-const lun = Math.floor(Math.random() * letras.length)
-console.log(lun);
-console.log(lun.valueOf());
-*/
